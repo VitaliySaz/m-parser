309e4af13d4d14434cb34518f717c9f945bdb604
Vitaliy Sazonov <1111vitalijj@gmail.com>
2023-02-22 17:58:56 +0200

    An asynchronous makeup parser was created

--- parser.py
+++ parser.py
@@ -9,95 +9,111 @@ import aiohttp
 import requests
 from bs4 import BeautifulSoup
 
-from utils import get_links_id
-
-asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-
-headers = {
-    'Content type': 'text/javascript; charset=utf-8',
-    'Accept': 'application/json, text/javascript, */*; q=0.01; charset=utf-8',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
-    'X-Requested-With': 'XMLHttpRequest'
-}
-
-
-@dataclass
 class Item:
-    id: int
-    value: str
-    img: str
-    price: str
-    price_old: str
-    checked: bool
-    wished: bool
+    def __init__(self, product_id: str,  item: dict):
+        self.product_id = product_id
+        self.item_id: Optional[int] = None
+        self.item_value: Optional[str] = None
+        self.item_value_with_wh: Optional[str] = None
+        self.item_img: Optional[str] = None
+        self.item_thumb: Optional[str] = None
+        self.item_price: Optional[int] = None
+        self.item_price_old: Optional[int] = None
+        self.item_checked: Optional[bool] = None
+        self.item_wished: Optional[bool] = None
+
+        for key, value in item.items():
+            setattr(self, 'item_'+key, value)
 
 
-@dataclass
-class Product:
-    _items_url = 'https://makeup.com.ua/ajax/product/{}/options/'
+class Products:
 
-    id: str
-    items: list = field(default_factory=list)
+    COUNT_PRODUCTS_IN_PAGE = 36
 
-    async def set_product_items(self) -> None:
-        print(self.id)
-        async with aiohttp.ClientSession() as session:
-            async with session.get(self._items_url.format(self.id), headers=headers) as resp:
-                if resp.status == 200:
-                    text = await resp.text()
-                    self.items.append(json.loads(text))
+    HEADERS = {
+        'Content type': 'text_html/javascript; charset=utf-8',
+        'Accept': 'application/json, text_html/javascript, */*; q=0.01; charset=utf-8',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Appl'
+                      'eWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+        'X-Requested-With': 'XMLHttpRequest'
+    }
 
-@dataclass
-class Pages:
-    __products_id = set()
+    PAGE_URL = 'https://makeup.com.ua/ajax/filter/'
 
-    url: str
-    count_pages: int
-
-    @property
-    def products_id(self) -> Set[str]:
-        for page in self._get_all_pages():
-            self.__products_id |= set(self._get_all_products_in_page(page))
-        return self.__products_id
-
-    def _get_all_pages(self) -> Iterable[str]:
-        response = requests.get(self.url)
-        yield response.text
+    def __init__(self, category: str, page_count: int):
+        self.category = category
+        self.page_count = page_count
+        self.products_id = set()
 
     @staticmethod
-    def _get_all_products_in_page(page: str) -> Iterable[str]:
-        soup = BeautifulSoup(page, 'html.parser')
-        for a in soup.find_all('a'):
+    def _get_page_products_id(text_html: str):
+        text_soup = BeautifulSoup(text_html, 'html.parser')
+        for a in text_soup.find_all('a'):
             link = a.get('href')
             if 'product' in link:
-                yield link.split('/')[3]
+                yield link.split('/')[2]
+
+    def _get_page_data(self, page_num: int) -> dict:
+        return {
+            'offset': page_num * self.COUNT_PRODUCTS_IN_PAGE,
+            'click': 'pager',
+            'categoryID': self.category
+        }
+
+    async def _get_page_text(self, page_num: int) -> str:
+        async with aiohttp.ClientSession() as session:
+            async with session.post(
+                    self.PAGE_URL,
+                    data=self._get_page_data(page_num),
+                    headers=self.HEADERS
+            ) as resp:
+                if resp.status == 200:
+                    return json.loads(await resp.text())['products']
+
+    def _set_products_id(self, pages: str):
+        for page in pages:
+            self.products_id |= set(self._get_page_products_id(page))
 
 
-@dataclass
-class Pars:
-    __products = list()
+class ProductItem(Products):
 
-    url: str
+    ITEM_URL = 'https://makeup.com.ua/ajax/product/{}/options/'
 
-    @property
-    def products(self) -> List[Product]:
-        return self.__products
+    def __init__(self, category: str, page_count: int):
+        super().__init__(category, page_count)
+        self.items: List[Item] = []
 
-    def get_count_pages(self) -> int:
-        return 1
+    async def set_product_items(self, product_id: str) -> None:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.ITEM_URL.format(product_id), headers=self.HEADERS) as resp:
+                if resp.status == 200:
+                    items = json.loads(await resp.text())
+                    self.items.extend([Item(product_id, i) for i in items])
 
-    def set_products(self) -> None:
-        pages = Pages(self.url, self.get_count_pages())
-        for product_id in pages.products_id:
-            self.products.append(Product(product_id))
-        event_loop = asyncio.get_event_loop()
-        event_loop.run_until_complete(
-            asyncio.gather(*[p.set_product_items() for p in self.products]))
-        event_loop.close()
+
+class Parser(ProductItem):
+
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    event_loop = asyncio.get_event_loop()
+
+    async def _await_product_items_tasks(self):
+        tasks = [self.set_product_items(task) for task in self.products_id]
+        await asyncio.gather(*tasks)
+
+    async def _await_page_text(self):
+        tasks = [self._get_page_text(num) for num in range(self.page_count)]
+        return await asyncio.gather(*tasks)
+
+    def run(self):
+        pages = self.event_loop.run_until_complete(self._await_page_text())
+        self._set_products_id(pages)
+        self.event_loop.run_until_complete(self._await_product_items_tasks())
+        self.event_loop.close()
 
 
 if __name__ == '__main__':
-
-    pars = Pars('https://makeup.com.ua/ua/categorys/3/')
-    pars.set_products()
-
+    start = time.time()
+    pars = Parser(category='3', page_count=10)
+    pars.run()
+    print(len(pars.items))
+    print(time.time() - start)
