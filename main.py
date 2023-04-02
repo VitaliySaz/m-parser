34377b81ac4c59b668479362ebe4862d388b9e33
Vitaliy Sazonov <1111vitalijj@gmail.com>
2023-04-02 21:31:17 +0300

    instead test of function to_history() created decorator time_manager()

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from typing import NamedTuple, Optional, List
 
 import telegram
 
-
 import pars
 from compare import CompareManagerHistory, ua_eu_strategy, CompareManagerItem
 from data import ItemMakeup
@@ -16,23 +15,33 @@ from utils import get_items_obj_dict
 
 class ManagerSettings(NamedTuple):
     pars: List[dict]
-    delta_limit: int = 20
+    delta_limit: int = 30
     timedelta: dict = {'seconds': 5}
-    set_to_db: dict = {'seconds': 20}
+    set_to_db: dict = {'seconds': 10}
 
-timedelta: Optional[datetime.timedelta] = None
-future = None
+
 settings: Optional[ManagerSettings] = None
 
+
+def time_manager(timedelta):
+    future = None
+
+    def wrapper(func):
+        def decorate(*args, **kwargs):
+            nonlocal future
+            td = datetime.timedelta(**timedelta)
+            now = datetime.datetime.now()
+            if not future:
+                future = now + td
+            remaining_seconds = (future - now).total_seconds()
+            if remaining_seconds < 0:
+                func(*args, **kwargs)
+                future = now + td
+        return decorate
+    return wrapper
+
 def to_history():
-    global settings, future
-    now = datetime.datetime.now()
-    if not future:
-        future = now + timedelta
-    remaining_seconds = (future - now).total_seconds()
-    if remaining_seconds < 0:
-        print('save the data')
-        future = now + timedelta
+    print('save the data')
 
 
 async def main1(data):
@@ -40,20 +49,22 @@ async def main1(data):
     item_list = await pars.get_items(data)
     print('end pars')
     items_obj_dict = get_items_obj_dict(item_list, ItemMakeup)
-    manager = CompareManagerItem(items_obj_dict, items_obj_dict)
+    manager = CompareManagerHistory(items_obj_dict)
     compare_item = manager.to_compare(ua_eu_strategy)
     # print(set(compare_item))
     limit = settings.delta_limit
-    for r in filter(lambda x: x > -limit, sorted(compare_item, reverse=True)):
+    for r in filter(lambda x: x > limit, sorted(compare_item, reverse=True)):
         # bot = telegram.Bot(token='token')
         # await bot.send_message(chat_id='1000612443', text=str(r))
         print(r)
-    print('#'*10)
+    print('#' * 10)
+    res = time_manager(settings.set_to_db)
+    dec = res(manager.add_to_history)
+    dec()
 
-    to_history()
 
 async def handle_client(reader, writer):
-    global settings, timedelta
+    global settings
     try:
         while True:
             data = await reader.read(1024)
@@ -62,7 +73,6 @@ async def handle_client(reader, writer):
             try:
                 res = json.loads(data.decode())
                 settings = ManagerSettings(**res)
-                timedelta = datetime.timedelta(**settings.set_to_db)
                 print('value is obtained', settings)
                 while True:
                     for pars_data in settings.pars:
@@ -78,18 +88,16 @@ async def handle_client(reader, writer):
     finally:
         writer.close()
 
+
 async def main():
     server = await asyncio.start_server(handle_client, 'localhost', 6060)
     async with server:
         await server.serve_forever()
+
 
 def run():
     asyncio.run(main())
 
 
 if __name__ == '__main__':
-
     run()
-
-
-
